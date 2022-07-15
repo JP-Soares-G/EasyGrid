874f511ba033e4a3d78bee026efae9010b6516d6
JPedro-SG <81583744+JPedro-SG@users.noreply.github.com>
2022-07-14 22:22:50 -0300

    downgrade react 17

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 // import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 // dotenv.config()
 import './index.css';
@@ -9,12 +9,22 @@ import {Provider} from 'react-redux'
 import axios from 'axios'
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
+/* root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
+);
+*/
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
